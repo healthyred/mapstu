26301445772bc4b3222412a1e3bc88309fd8d7e9
jian13579 <jianhlu018@gmail.com>
2016-12-23 22:21:14 -0500

    initial changes

--- regions.R
+++ regions.R
@@ -1,2 +1,11 @@
 #this is the script to describe the classes of the regions
 #plans to take in the data set
+install.packages("tmap")
+install.packages("leaflet")
+library("tmap")
+library("leaflet")
+
+
+usshapefile <- "data/cb_2014_us_county_5m/cb_2014_us_county_5m.shp"
+usgeo <- read_shape(file=usshapefile)
+

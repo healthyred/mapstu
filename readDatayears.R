8da03ef9b71caeeba8515a070a31a23d0047df04
jian13579 <jhl3@williams.edu>
2016-12-24 21:12:24 -0500

    Update readDatayears.R

--- readDatayears.R
+++ readDatayears.R
@@ -1,25 +1,21 @@
 
 #Modifying the text file to get the number of students coming into Williams per year per country/state into a readable csv file
-
+#the text file must have the format (Country Value) and not separated by a new line
 readDatayears <- function(dataset){
   input <- readLines(dataset, warn = FALSE)
   #targets the entire string that we want w/ 3 cases where countries have 1, 2, or 3 names
-  regone= "[a-zA-Z]+ [0-9]+"
-  regtwo= "[a-zA-Z]* [a-zA-Z]+ [0-9]+"
-  regthree= "[a-zA-Z]* [a-zA-Z]* [a-zA-Z]+ [0-9]+"
+  #regone= "[a-zA-Z]+ [0-9]+"
+  #regtwo= "[a-zA-Z]* [a-zA-Z]+ [0-9]+"
+  #regthree= "[a-zA-Z]* [a-zA-Z]* [a-zA-Z]+ [0-9]+"
   m <- regexpr("[a-zA-Z]+ [0-9]+|[a-zA-Z]* [a-zA-Z]+ [0-9]+|[a-zA-Z]* [a-zA-Z]* [a-zA-Z]+ [0-9]+" , input)
   input <- regmatches(input, m)
-  #puts in a comma between each blank space
-  for(i in input){
-    if (gsub(regone,"", i == ""){
-      print(input[i])
-    }
-  }
-        input[i] <- gsub(" ", ",", input)
-    }
-  }
-    else if
-  }
+  #take out all blank spaces
+  input <- gsub(" ","",input)
+  #put a comma in between every letter and number
+  input <- gsub("([a-z])([0-9])", "\\1, \\2", input)
+  #puts a space between each lower case and capital letter
+  #not yet sure how to fix cases of Districtof and Trinidadand
+  input <- gsub("([a-z])([A-Z])", "\\1 \\2", input)
 }
 input
 #reset code

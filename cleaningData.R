c2f7e31e7f25f41718904c9d84fb6bd325006fe5
jian13579 <jianhlu018@gmail.com>
2016-12-28 02:08:57 -0500

    must work on dupicates

--- cleaningData.R
+++ cleaningData.R
@@ -39,22 +39,22 @@ readYears("~/HutchinHill/2015-2016.rtf", "2015.2016_Clean")
 
 #Renames all of the columns
 newname <- c("State/Countries", "Number of Students")
-names(`2000`) <- newname
-names(`2001`) <- newname
-names(`2002`) <- newname
-names(`2003`) <- newname
-names(`2004`) <- newname
-names(`2005`) <- newname
-names(`2006`) <- newname
-names(`2007`) <- newname
-names(`2008`) <- newname
-names(`2009`) <- newname
-names(`2010`) <- newname
-names(`2011`) <- newname
-names(`2012`) <- newname
-names(`2013`) <- newname
-names(`2014`) <- newname
-names(`2015`) <- newname
+names(`2000`) <- c("State/Countries", "Students Attending in 2000-2001")
+names(`2001`) <- c("State/Countries", "Students Attending in 2001-2002")
+names(`2002`) <- c("State/Countries", "Students Attending in 2002-2003")
+names(`2003`) <- c("State/Countries", "Students Attending in 2003-2004")
+names(`2004`) <- c("State/Countries", "Students Attending in 2004-2005")
+names(`2005`) <- c("State/Countries", "Students Attending in 2005-2006")
+names(`2006`) <- c("State/Countries", "Students Attending in 2006-2007")
+names(`2007`) <- c("State/Countries", "Students Attending in 2007-2008")
+names(`2008`) <- c("State/Countries", "Students Attending in 2008-2009")
+names(`2009`) <- c("State/Countries", "Students Attending in 2009-2010")
+names(`2010`) <- c("State/Countries", "Students Attending in 2010-2011")
+names(`2011`) <- c("State/Countries", "Students Attending in 2011-2012")
+names(`2012`) <- c("State/Countries", "Students Attending in 2012-2013")
+names(`2013`) <- c("State/Countries", "Students Attending in 2013-2014")
+names(`2014`) <- c("State/Countries", "Students Attending in 2014-2015")
+names(`2015`) <- c("State/Countries", "Students Attending in 2015-2016")
 
 #Creating the data frames for all of these lists
 year2000 <-data.frame(`2000`)
@@ -75,19 +75,21 @@ year2014 <-data.frame(`2014`)
 year2015 <-data.frame(`2015`)
 
 #adding a column specifying the year
-year2000$Year <- 2000
-year2001$Year <- 2001
-year2002$Year <- 2002
-year2003$Year <- 2003
-year2004$Year <- 2004
-year2005$Year <- 2005
-year2006$Year <- 2006
-year2007$Year <- 2007
-year2008$Year <- 2008
-year2009$Year <- 2009
-year2010$Year <- 2010
-year2011$Year <- 2011
-year2012$Year <- 2012
-year2013$Year <- 2013
-year2014$Year <- 2014
-year2015$Year <- 2015
+#year2000$Year <- 2000
+#year2001$Year <- 2001
+#year2002$Year <- 2002
+#year2003$Year <- 2003
+#year2004$Year <- 2004
+#year2005$Year <- 2005
+#year2006$Year <- 2006
+#year2007$Year <- 2007
+#year2008$Year <- 2008
+#year2009$Year <- 2009
+#year2010$Year <- 2010
+#year2011$Year <- 2011
+#year2012$Year <- 2012
+#year2013$Year <- 2013
+#year2014$Year <- 2014
+#year2015$Year <- 2015
+#Creates a total data frame
+totalframe <- rbind(year2000, year2001, year2002, year2003, year2004, year2005, year2006, year2007, year2008, year2009, year2010, year2011, year2012, year2013, year2014, year2015)

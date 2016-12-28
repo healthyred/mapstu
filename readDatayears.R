9757aa20fae06bc94e24859f3bc8dc1a85a934d7
jian13579 <jianhlu018@gmail.com>
2016-12-28 00:00:31 -0500

    finishing readYears

--- readDatayears.R
+++ readDatayears.R
@@ -4,12 +4,11 @@
 #the text file must have the format (Country Value) and not separated by a new line
 readDatayears <- function(dataset){
   input <- readLines(dataset, warn = FALSE)
-  #targets the entire string that we want w/ 3 cases where countries have 1, 2, or 3 names
+  #targets the entire string that we want w/ 3 cases where countries have up to 5 words in their name
   #regone= "[a-zA-Z]+ [0-9]+"
   #regtwo= "[a-zA-Z]* [a-zA-Z]+ [0-9]+"
   #regthree= "[a-zA-Z]* [a-zA-Z]* [a-zA-Z]+ [0-9]+"
-  #specialreg= "[a-zA-Z]*, [a-zA-Z]* [a-zA-Z]* [a-zA-Z]+ [0-9]+"
-  m <- regexpr("[a-zA-Z]+ [0-9]+|[a-zA-Z]* [a-zA-Z]+ [0-9]+|[a-zA-Z]* [a-zA-Z]* [a-zA-Z]+ [0-9]+|[a-zA-Z]*[a-zA-Z]* [a-zA-Z]* [a-zA-Z]+ [0-9]+|[a-zA-Z]*, [a-zA-Z]* [a-zA-Z]* [a-zA-Z]+ [0-9]+" , input)
+  m <- regexpr("[a-zA-Z]+ [0-9]+|[a-zA-Z]* [a-zA-Z]+ [0-9]+|[a-zA-Z]* [a-zA-Z]* [a-zA-Z]+ [0-9]+|[a-zA-Z]*[a-zA-Z]* [a-zA-Z]* [a-zA-Z]+ [0-9]+|[a-zA-Z]* [a-zA-Z]* [a-zA-Z]* [a-zA-Z]+ [0-9]+|[a-zA-Z]* [a-zA-Z]* [a-zA-Z]* [a-zA-Z]* [a-zA-Z]+ [0-9]+|[a-zA-Z]* [a-zA-Z]* [a-zA-Z]* [a-zA-Z]* [a-zA-Z]* [a-zA-Z]+ [0-9]+" , input)
   input <- regmatches(input, m)
   #take out all blank spaces
   input <- gsub(" ","",input)
@@ -20,15 +19,23 @@ readDatayears <- function(dataset){
   input <- gsub("([a-z])([A-Z])", "\\1 \\2", input)
 }
 input
-#works for the cases of 2000-2009, since at 2009-2010 Williams changes how they denote the values for one year
-#code for after 2009
-input <- gsub("[.]","",input)
-input <- gsub(" ","",input)
-input <- gsub("([a-z])([0-9])", "\\1, \\2", input)
-input <- gsub("([a-z])([A-Z])", "\\1 \\2", input)
-input <- gsub("([0-9])([A-Z])", "\\1 \\2", input)
+#Read years is a funtion that is a more general adaption of readDatayears and will not use all of the regex that was 
+#used to sort through most of the dataset
+readYears <- function(dataset){
+  input <- readLines(dataset, warn = FALSE)
+  #takes out all of the excess periods in every dataset
+  input <- gsub("[.]","",input)
+  #takes out all of the spaces in each dataset
+  input <- gsub(" ","",input)
+  #Inserts a comma between the country and its value
+  input <- gsub("([a-z])([0-9])", "\\1, \\2", input)
+  #Puts spaces between every start of a new word
+  input <- gsub("([a-z])([A-Z])", "\\1 \\2", input)
+  #Puts spaces in between the values in the words
+  input <- gsub("([0-9])([A-Z])", "\\1 \\2", input)
+}
 
 #reset code
 input <- readLines("~/HutchinHill/2000-2001.rtf")
-m <- regexpr("[a-zA-Z]+ [0-9]+|[a-zA-Z]* [a-zA-Z]+ [0-9]+|[a-zA-Z]* [a-zA-Z]* [a-zA-Z]+ [0-9]+|[a-zA-Z]*[a-zA-Z]* [a-zA-Z]* [a-zA-Z]+ [0-9]+|[a-zA-Z]*, [a-zA-Z]* [a-zA-Z]* [a-zA-Z]+ [0-9]+" , input)
+m <- regexpr("[a-zA-Z]+ [0-9]+|[a-zA-Z]* [a-zA-Z]+ [0-9]+|[a-zA-Z]* [a-zA-Z]* [a-zA-Z]+ [0-9]+|[a-zA-Z]*[a-zA-Z]* [a-zA-Z]* [a-zA-Z]+ [0-9]+|[a-zA-Z]* [a-zA-Z]* [a-zA-Z]* [a-zA-Z]+ [0-9]+|[a-zA-Z]* [a-zA-Z]* [a-zA-Z]* [a-zA-Z]* [a-zA-Z]+ [0-9]+|[a-zA-Z]* [a-zA-Z]* [a-zA-Z]* [a-zA-Z]* [a-zA-Z]* [a-zA-Z]+ [0-9]+" , input)
 input <- regmatches(input, m)

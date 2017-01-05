e6f8fd454f8646dc1f056b29b8d504d03c0a2289
jian13579 <jianhlu018@gmail>
2017-01-04 23:33:30 -0500

    created color

--- regions.R
+++ regions.R
@@ -5,6 +5,17 @@ install.packages("leaflet")
 library("tmap")
 library("leaflet")
 
+#a function that takes in present and past which are vectors of the dataframe and calculates the absolute change of students
+#that come to Williams from each state/country
+absolutechange <- function(present , past, name){
+  present-past
+}
+
+#Create a dataframe with the countries and the colors attached to each country dependent on the absolute change
+color <- function(){
+
+}
+
 
 usshapefile <- "data/cb_2014_us_county_5m/cb_2014_us_county_5m.shp"
 usgeo <- read_shape(file=usshapefile)

bf207118a17f93e119ac9212b32eaaecac618a78
jian13579 <jianhlu018@gmail.com>
2017-01-12 22:27:31 -0500

    changes to data

--- regions.R
+++ regions.R
@@ -2,9 +2,11 @@
 #plans to take in the data set
 install.packages("tmap")
 install.packages("leaflet")
+install.packages("stringdist")
 library("tmap")
 library("leaflet")
-
+library("tmaptools")
+library("stringdist")
 #a function that takes in present and past which are vectors of the dataframe and calculates the absolute change of students
 #that come to Williams from each state/country
 #absolutechange <- function(present , past){
@@ -118,9 +120,48 @@ year2012.2014$Change <- c(yearsdata$Students.Attending.in.2013.2014- yearsdata$S
 year2013.2015$Change <- c(yearsdata$Students.Attending.in.2014.2015- yearsdata$Students.Attending.in.2013.2014)
 year2014.2016$Change <- c(yearsdata$Students.Attending.in.2015.2016- yearsdata$Students.Attending.in.2014.2015)
 
+#Testing what happens in the colors are set to NULL
+#Assigns the color to each state for each year
+year2000.2002$Color <- sapply(yearsdata$Students.Attending.in.2001.2002- yearsdata$Students.Attending.in.2000.2001,color)
+year2001.2003$Color <- sapply(yearsdata$Students.Attending.in.2002.2003- yearsdata$Students.Attending.in.2001.2002,color)
+year2002.2004$Color <- sapply(yearsdata$Students.Attending.in.2003.2004- yearsdata$Students.Attending.in.2002.2003,color)
+year2003.2005$Color <- sapply(yearsdata$Students.Attending.in.2004.2005- yearsdata$Students.Attending.in.2003.2004,color)
+year2004.2006$Color <- sapply(yearsdata$Students.Attending.in.2005.2006- yearsdata$Students.Attending.in.2004.2005,color)
+year2005.2007$Color <- sapply(yearsdata$Students.Attending.in.2006.2007- yearsdata$Students.Attending.in.2005.2006,color)
+year2006.2008$Color <- sapply(yearsdata$Students.Attending.in.2007.2008- yearsdata$Students.Attending.in.2006.2007,color)
+year2007.2009$Color <- sapply(yearsdata$Students.Attending.in.2008.2009- yearsdata$Students.Attending.in.2007.2008,color)
+year2008.2010$Color <- sapply(yearsdata$Students.Attending.in.2009.2010- yearsdata$Students.Attending.in.2008.2009,color)
+year2009.2011$Color <- sapply(yearsdata$Students.Attending.in.2010.2011- yearsdata$Students.Attending.in.2009.2010,color)
+year2010.2012$Color <- sapply(yearsdata$Students.Attending.in.2011.2012- yearsdata$Students.Attending.in.2010.2011,color)
+year2011.2013$Color <- sapply(yearsdata$Students.Attending.in.2012.2013- yearsdata$Students.Attending.in.2011.2012,color)
+year2012.2014$Color <- sapply(yearsdata$Students.Attending.in.2013.2014- yearsdata$Students.Attending.in.2012.2013,color)
+year2013.2015$Color <- sapply(yearsdata$Students.Attending.in.2014.2015- yearsdata$Students.Attending.in.2013.2014,color)
+year2014.2016$Color <- sapply(yearsdata$Students.Attending.in.2015.2016- yearsdata$Students.Attending.in.2014.2015,color)
+
+#Adds the absolutechange colomn to each data frame
+year2000.2002$Color <- NULL
+year2001.2003$Color <- NULL
+year2002.2004$Color <- NULL
+year2003.2005$Color <- NULL
+year2004.2006$Color <- NULL
+year2005.2007$Color <- NULL
+year2006.2008$Color <- NULL
+year2007.2009$Color <- NULL
+year2008.2010$Color <- NULL
+year2009.2011$Color <- NULL
+year2010.2012$Color <- NULL
+year2011.2013$Color <- NULL
+year2012.2014$Color <- NULL
+year2013.2015$Color <- NULL
+year2014.2016$Color <- NULL
 
 
 usshapefile <- "~/HutchinHill/cb_2015_us_state_5m/cb_2015_us_state_5m.shp"
 usgeo <- tmaptools::read_shape(file = usshapefile)
 tmap::qtm(usgeo, fill = )
 usmap <- tmaptools::append_data(usgeo , year2000.2002 , key.shp = "NAME", key.data = "Color", ignore.duplicates = TRUE, ignore.na = TRUE)
+
+nationshapefile <- "~/HutchinHill/cb_2015_us_state_20m/cb_2015_us_state_20m.shp"
+nationgeo<- read_shape(file = nationshapefile)
+
+http://gis.stackexchange.com/questions/176722/how-to-join-a-table-to-a-shapefile-with-non-matching-ids-and-names-similar-stri

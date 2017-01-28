db4ff8a6ade384a73f52e22d7266fca801b8d946
jian13579 <jianhlu018@gmail.com>
2017-01-28 16:51:15 -0500

    started package description

---
+++ R/mapstu-package.R
@@ -0,0 +1,20 @@
+#' The mapstu package
+#'
+#' \tabular{ll}{ Package \tab mapstu \cr Type: \tab Package\cr
+#' Version: \tab 0.1-1\cr Date: \tab 2017-01-23\cr License: \tab GPL-3\cr }
+#'
+#' The mapstu package uses the geographical distribution data of the entire
+#' Williams College student population published in the Williams Course
+#' Catalogs to compare which states, and countries students are from each
+#' year. This is done for fifteen years from 2000 to 2015 by pulling data
+#' from text versions of the geographical distribution data. The core
+#' components of the package creates different cloropleth maps based on
+#' the arguments passed to the function. With the visual support of
+#' the cloropleth map, one can easily visualize the difference of
+#' student geographical distribution between any two years of the data
+#' set. A printed comparison of the two years can also produced.
+#'
+#' @name mapstu-package.R
+#' @docType package
+#'
+NULL
